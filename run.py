2750863d9ef83929820000b6d4301d9722d192bc
thomas <psychoid@hotmail.fr>
2020-08-07 12:06:46 +0200

    probleme

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('/page_GrandPy.html')
+    return render_template('templates/page_GrandPy.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
