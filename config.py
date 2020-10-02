35970877d7069747f20317b6280b238f35011e4a
thomas <psychoid@hotmail.fr>
2020-10-02 15:57:19 +0200

    pffff

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 # To use the googlemap API we must to generate a secret key:
 
-GOOGLEMAP_API_KEY = os.environ["AIzaSyDK8bpH2cj2iZ4xxVuBj1vguTS92b4gmdY"]
+GOOGLEMAP_API_KEY = os.environ["GOOGLEMAP_API_KEY"]
